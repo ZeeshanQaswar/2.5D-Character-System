96d6a63fdb52ff16fe9cdada630b67fdbb7f0f45
ZEESHANQASWA-PC\Zeeshan.Qaswar <zeeshan.qaswar@geniteam.pk>
2021-01-19 12:12:45 +0500

    Working on ledgeGraba Mechanics & Sliding Mechanics.

---
+++ Assets/Scripts/FeetIK.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeetIK : MonoBehaviour
+{
+
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        
+    }
+
+}
